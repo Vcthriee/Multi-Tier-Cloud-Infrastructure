891f367bfb23cf5e200d54f8fe4e61636efbc5cb
vcthriee <vcthrieearikpo@gmail.com>
2026-02-21 00:34:55 +0100

    backend-edited

--- envs/dev/backend.tf
+++ envs/dev/backend.tf
@@ -1,9 +1,10 @@
+
 terraform {
   backend "s3" {
     bucket         = "vcthriee-terraform-states"
     key            = "cloud-platform/dev/terraform.tfstate"
     region         = "af-south-1"
-    dynamodb_table = "terraform-state-locks"
+    use_lockfile = true
     encrypt        = true
   }
 }
